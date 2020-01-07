a6bbd24ba533a47db341570c59e6eca06f8ece52
Yelz <yscbb2011@gmail.com>
2020-01-07 14:52:03 +0000

    Thrid-person cam & movement

---
+++ Assets/Script/C_Controller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C_Controller : MonoBehaviour
+{
+    public float Speed;
+
+
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement() {
+        //front & back
+        float hor = Input.GetAxis("Horizontal");
+        //left and right
+        float ver = Input.GetAxis("Vertical");
+
+        Vector3 playerMovement = new Vector3(hor, 0f, ver) * Speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+    }
+}
